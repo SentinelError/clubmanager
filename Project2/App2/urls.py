fe86f9a2871c0c506ca40172cfe4fa55167b0180
SentinelError <rkgva2001@gmail.com>
2022-07-21 00:53:19 +0530

    First Commit

---
+++ Project2/App2/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from . import views
+
+app_name = "main"
+
+
+urlpatterns = [
+    path("", views.homepage, name="homepage"),
+    path("register", views.register_request, name="register"),
+    path("edituser", views.edit_request, name="edit"),
+    path("login", views.login_request, name="login"),
+    path("logout", views.logout_request, name= "logout"),
+    path("clubhomepage", views.clubhomepage, name="clubhomepage"),
+    path("events", views.events, name="events" ),
+    path("addevent", views.addevents, name="addevent" ),
+    path("updateevents/<eventid>", views.updevents, name="updateevents" ),
+    path("deleteevent/<eventid>", views.delevents, name="delevent" ),
+    path("venues", views.venues, name="venues"),
+    path("updatevenues/<venueid>", views.updvenues, name="updatevenues" ),
+    path("deletevenue/<venueid>", views.delvenues, name="delvenue" ),
+    path("addvenue", views.addvenues, name="addvenue" ),
+    path("reports", views.reports, name="reports" ),
+    path("addreport", views.addreport, name="addreport" ),
+    path("updatereport/<reportid>", views.updreport, name="updatereport" ),
+    path("deletereport/<reportid>", views.delreport, name="delreport" ),
+    path("password/", views.changepassword, name="changepassword" ),
+]
---
+++ Project2/App2/views.py
@@ -0,0 +1,184 @@
+from django.shortcuts import  render, HttpResponseRedirect
+from .forms import NewUserForm, EventFormA ,EventFormS, VenueForm, StudentForm, ReportForm, NewEditForm
+from django.contrib.auth import login, authenticate, logout, update_session_auth_hash
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from .models import Event,Venue,Report
+
+
+def register_request(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		Sform = StudentForm(request.POST)
+
+		if form.is_valid() and Sform.is_valid():
+			user = form.save()
+
+			student = Sform.save(commit=False)
+			student.user = user
+
+			student.save()
+
+			#login(request, user)
+			messages.success(request, "Registration successful." )
+			return HttpResponseRedirect("/login")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	Sform = StudentForm()
+	return render (request=request, template_name="App2/register.html", context={"register_form":form, "Student":Sform})
+
+def edit_request(request):
+	if request.method == "POST":
+		form = NewEditForm (request.POST, instance=request.user)
+		if form.is_valid():
+			user = form.save()
+			return HttpResponseRedirect("/")
+	else:
+		form = NewEditForm(instance=request.user)
+	return render(request=request, template_name="App2/edituser.html",context={"edit_form": form,})
+
+def changepassword(request):
+	if request.method == "POST":
+		form = PasswordChangeForm(data=request.POST, user=request.user)
+		if form.is_valid():
+			user = form.save()
+			update_session_auth_hash(request, form.user)
+			messages.success(request, "Registration successful.")
+			return HttpResponseRedirect("/")
+	else:
+		form = PasswordChangeForm(user=request.user)
+	return render(request=request, template_name="App2/passwordchange.html",context={"passwordchange_form": form,})
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				return HttpResponseRedirect('/clubhomepage')
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="App2/login.html", context={"login_form":form})
+
+def logout_request(request):
+	logout(request)
+
+	return render(request=request, template_name='App2/logout.html')
+
+def homepage(request):
+	return render(request=request, template_name='App2/homepage.html')
+
+def clubhomepage(request):
+	return render(request=request, template_name='App2/clubhomepage.html')
+
+def events(request):
+	event1 = Event.objects.all().order_by('event_date')
+	return render(request, 'App2/events.html', {'event1': event1})
+
+
+def updevents(request, eventid):
+	event = Event.objects.get(pk=eventid)
+	if request.user.is_superuser:
+		form = EventFormA(request.POST or None, instance=event)
+	else:
+		form = EventFormS(request.POST or None, instance=event)
+
+	if form.is_valid():
+		form.save()
+		return HttpResponseRedirect('/events')
+
+	return render(request, 'App2/updateevents.html',
+				  {'event': event,
+				   'form': form})
+
+def addevents(request):
+	initial_data ={
+		'name':'Training'
+	}
+	if request.method == 'POST':
+		if request.user.is_superuser:
+			form = EventFormA(request.POST)
+		else:
+			form = EventFormS(request.POST)
+
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/events')
+	if request.user.is_superuser:
+		form = EventFormA()
+	else:
+		form = EventFormS(initial=initial_data)
+	return render(request=request, template_name='App2/addevent.html', context={'form':form,})
+
+def delevents(request,eventid):
+	event = Event.objects.get(pk=eventid)
+	event.delete()
+	return HttpResponseRedirect('/events')
+
+def venues(request):
+	venue1 = Venue.objects.all()
+	return render(request, 'App2/venues.html', {'venue1': venue1})
+
+def updvenues(request, venueid):
+	venue = Venue.objects.get(pk=venueid)
+	form = VenueForm(request.POST or None, instance=venue)
+
+	if form.is_valid():
+		form.save()
+		return HttpResponseRedirect('/venues')
+
+	return render(request, 'App2/updatevenues.html',
+				  {'venue': venue,
+				   'form': form})
+
+def delvenues(request,venueid):
+	venue = Venue.objects.get(pk=venueid)
+	venue.delete()
+	return HttpResponseRedirect('/venues')
+
+def addvenues(request):
+
+	if request.method == 'POST':
+		form = VenueForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/venues')
+	form = VenueForm()
+	return render(request=request, template_name='App2/addvenue.html', context={'form':form,})
+
+def reports(request):
+	report1 = Report.objects.all()
+	return render(request, 'App2/reports.html', {'report1':report1})
+
+def addreport(request):
+
+	if request.method == 'POST':
+		form = ReportForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/reports')
+	form = ReportForm()
+	return render(request=request, template_name='App2/addreport.html', context={'form':form,})
+
+def updreport(request, reportid):
+	report = Report.objects.get(pk=reportid)
+	form = ReportForm(request.POST or None, instance=report)
+
+	if form.is_valid():
+		form.save()
+		return HttpResponseRedirect('/reports')
+
+	return render(request, 'App2/updatereport.html',
+				  {'report': report,
+				   'form': form})
+
+def delreport(request,reportid):
+	report = Report.objects.get(pk=reportid)
+	report.delete()
+	return HttpResponseRedirect('/reports')
